f26f2ebe66073f67a25884a98e3e35e600fd5099
stanimirovic <markostanimirovic95@gmail.com>
2020-07-26 02:41:57 +0200

    improved publish

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,11 +1,27 @@
-const { exec, execSync } = require('child_process');
+const { execSync } = require('child_process');
+const { writeFileSync } = require('fs');
 const { join } = require('path');
 
 const isCodeCommitted = execSync('git diff').toString().length === 0;
 if (!isCodeCommitted) throw new Error('Please commit or stash changes before publish!');
 
-const distPath = join(__dirname, '..', 'dist');
+const rootPath = join(__dirname, '..');
+const distPath = join(rootPath, 'dist');
 const julietteDistPath = join(distPath, 'juliette');
 const julietteNgDistPath = join(distPath, 'juliette-ng');
+const readmePath = join(rootPath, 'README.md');
+const projectInfoPath = join(rootPath, 'project-info.json');
+const juliettePackageJsonPath = join(julietteDistPath, 'package.json');
+const julietteNgPackageJsonPath = join(julietteNgDistPath, 'package.json');
 
-exec(`cd ${julietteDistPath} && npm publish && cd ${julietteNgDistPath} && npm publish`).stdout.pipe(process.stdout);
+const projectInfo = require(projectInfoPath);
+const juliettePackageJson = require(juliettePackageJsonPath);
+const julietteNgPackageJson = require(julietteNgPackageJsonPath);
+
+console.log('copying README...');
+execSync(`cp -r ${readmePath} ${julietteDistPath} && cp -r ${readmePath} ${julietteNgDistPath}`, { stdio: 'inherit' });
+console.log('copying project.info...');
+writeFileSync(juliettePackageJsonPath, JSON.stringify({ ...juliettePackageJson, ...projectInfo }, null, 2));
+writeFileSync(julietteNgPackageJsonPath, JSON.stringify({ ...julietteNgPackageJson, ...projectInfo }, null, 2));
+console.log('publishing...');
+execSync(`cd ${julietteDistPath} && npm publish && cd ${julietteNgDistPath} && npm publish`, { stdio: 'inherit' });
