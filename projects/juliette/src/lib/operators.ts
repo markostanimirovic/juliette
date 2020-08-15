84fb28ee239f9c9ad1e6148dfb03e6c7ce092415
stanimirovic <markostanimirovic95@gmail.com>
2020-08-15 02:45:54 +0200

    removed surplus signatures of ofType operator

--- projects/juliette/src/lib/operators.ts
+++ projects/juliette/src/lib/operators.ts
@@ -2,18 +2,6 @@ import { filter, map } from 'rxjs/operators';
 import { MonoTypeOperatorFunction, OperatorFunction } from 'rxjs';
 import { Handler, HandlerCreator } from './models';
 
-export function ofType(
-  handlerCreator: HandlerCreator,
-): OperatorFunction<Handler<any, any>, Handler>;
-
-export function ofType<P>(
-  handlerCreator: HandlerCreator<null, P>,
-): OperatorFunction<Handler<any, any>, Handler<null, P>>;
-
-export function ofType<S>(
-  handlerCreator: HandlerCreator<S>,
-): OperatorFunction<Handler<any, any>, Handler<S>>;
-
 export function ofType<S, P>(
   handlerCreator: HandlerCreator<S, P>,
 ): OperatorFunction<Handler<any, any>, Handler<S, P>>;
